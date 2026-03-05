544a43d37509e085ff918cf4a4c3ddd668f4092e
MasterBhuvnesh <bhuvneshverma2904@gmail.com>
2026-03-06 01:10:13 +0530

    fix: basePath for github pages

--- changelog/next.config.ts
+++ changelog/next.config.ts
@@ -3,8 +3,12 @@ import { createMDX } from "fumadocs-mdx/next"
 
 const withMDX = createMDX()
 
+const repo = "system.design"
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   images: { unoptimized: true },
   transpilePackages: ["geist"],
 }
